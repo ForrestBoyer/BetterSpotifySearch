34b64ad1e6e6b7c67c9e1094bbf0a0eeccc77512
fboyer <forrestboyer2@gmail.com>
2024-02-26 14:10:09 -0500

    adding different front end pages and navigation

--- BetterSpotifySearch/src/app/app.routes.ts
+++ BetterSpotifySearch/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SongFinderComponent } from './song-finder/song-finder.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: '/songfinder', component: SongFinderComponent},
+    { path: "**", component: HomeComponent }
+];
---
+++ BetterSpotifySearch/src/app/song-finder/song-finder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-finder',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-finder.component.html',
+  styleUrl: './song-finder.component.scss'
+})
+export class SongFinderComponent {
+
+}
