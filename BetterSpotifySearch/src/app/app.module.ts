144f655c141563f9207614f63f906181998aacef
Forrest Boyer <forrestboyer2@gmail.com>
2024-03-12 22:36:50 -0400

    buttons go to places

--- BetterSpotifySearch/src/app/app.module.ts
+++ BetterSpotifySearch/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component'; 
+import { LoginComponent } from './login/login.component'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularMaterialModule } from './angular-material.module';
@@ -11,7 +13,9 @@ import { AngularMaterialModule } from './angular-material.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
