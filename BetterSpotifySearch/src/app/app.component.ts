fed8caea54908cc0e5b151641589c27a50e4af8c
Forrest Boyer <forrestboyer2@gmail.com>
2024-03-12 21:53:54 -0400

    New angular version everybody

--- BetterSpotifySearch/src/app/app.component.ts
+++ BetterSpotifySearch/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { MatButton } from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavMenuComponent, MatButton],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'BetterSpotifySearch';
--- BetterSpotifySearch/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { SongFinderComponent } from './song-finder/song-finder.component';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: '/songfinder', component: SongFinderComponent},
-    { path: "**", component: HomeComponent }
-];
