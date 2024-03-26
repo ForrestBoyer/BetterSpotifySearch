7626298117b5e29876806c4e922f5838c690bb9f
Tameron Honnellio <tameronhonnellio@gmail.com>
2024-03-25 21:48:59 -0400

    updated front end

--- BetterSpotifySearch/src/app/angular-material.module.ts
+++ BetterSpotifySearch/src/app/angular-material.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     imports: [
       MatToolbarModule,
-      MatIconModule
+      MatIconModule,
+      MatFormFieldModule,
+      MatCardModule,
+      MatButtonModule
     ],
     exports: [
       MatToolbarModule,
-      MatIconModule
+      MatIconModule,
+      MatFormFieldModule,
+      MatCardModule,
+      MatButtonModule
+    ],
+    providers: [
+      {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
     ]
   })
   export class AngularMaterialModule { }
--- BetterSpotifySearch/src/app/login/login.component.ts
+++ BetterSpotifySearch/src/app/login/login.component.ts
@@ -12,4 +12,8 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  authenticateUser(): void {
+    console.log("Authenticated!");
+  }
+
 }
