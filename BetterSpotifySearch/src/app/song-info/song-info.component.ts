f39999af7e34d42ca8c2e63b61390da147b3eeb1
Tameron Honnellio <tameronhonnellio@gmail.com>
2024-03-27 18:28:58 -0400

    Update song-info.component.ts

--- BetterSpotifySearch/src/app/song-info/song-info.component.ts
+++ BetterSpotifySearch/src/app/song-info/song-info.component.ts
@@ -15,7 +15,6 @@ export class SongInfoComponent implements OnInit {
   }
 
   setSongName(event: any): void {
-    // Called from html on input (don't know how to pass event into here)
     this.songName = event.target.value;
     console.log("Song name set", this.songName);
   }
@@ -24,6 +23,7 @@ export class SongInfoComponent implements OnInit {
     // api call for song 
     // run register: http://localhost:40080/api/SpotifyAuthentication/Register
     // GET http://localhost:40080/api/Song/Search/{{songName}}
+    // Pass song name and ID to info results page and do the api search there to extract song data
     // GET http://localhost:40080/api/Song/Features/{{songID}}
     console.log("Song info request: ", this.songName);
   }
