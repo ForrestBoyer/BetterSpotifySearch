d4de8c0b203707c45f772d7c17eeeae0f6e1d43e
Kyran C <71733736+nary-rgb@users.noreply.github.com>
2024-03-27 20:17:00 -0400

    added value based tests for searches

--- BetterSpotifySearchAPI.Tests/ArtistController_Tests.cs
+++ BetterSpotifySearchAPI.Tests/ArtistController_Tests.cs
@@ -1,6 +1,7 @@
 using BetterSpotifySearchAPI.Controllers;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using Moq;
 
 namespace BetterSpotifySearchAPI.Tests;
@@ -20,5 +21,21 @@ public class ArtistController_Tests{
         responseContent = castResponse.Value.ToString();
 
         Assert.Equal("No search value", responseContent);
+    }
+
+    [Fact]
+    public async void NameSearchReturnsQueen()
+    {
+        var accessService = new AccessService();
+
+        var controller = new ArtistController(accessService);
+
+        var response = await controller.Search("queen");
+        var castResponse = response as OkObjectResult;
+        var responseContent = castResponse.Value as string;
+        var jsonContent = JObject.Parse(responseContent);
+        var name = jsonContent["artists"]["items"][0]["name"];
+
+        Assert.Equal("Queen", name);
     }
 }
--- BetterSpotifySearchAPI.Tests/SongController_Tests.cs
+++ BetterSpotifySearchAPI.Tests/SongController_Tests.cs
@@ -1,6 +1,7 @@
 using BetterSpotifySearchAPI.Controllers;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using Moq;
 
 namespace BetterSpotifySearchAPI.Tests;
@@ -39,6 +40,25 @@ public class SongController_Tests
     }
 
     [Fact]
+    public async void SearchByFeatureResultMatches()
+    {
+        var accessService = new AccessService();
+        var controller = new SongController(accessService);
+
+        var response = await controller.SearchBy(null, "hip-hop", null, null, null, null, null, null, 0.911,
+                                                 null, null, null, null, null, 0.566, null, null, null, 
+                                                 null, null, null, null, null, null, null, null, null, 
+                                                 null, null, null, null, null, null, null, null, 0.135,
+                                                 null, null, 150, null, null, null, null, null, null);
+        var castResponse = response as OkObjectResult;
+        var responseContent = castResponse.Value as string;
+        var jsonContent = JObject.Parse(responseContent);
+        var name = jsonContent["tracks"][0]["name"];
+
+        Assert.Equal("Leave Me", name);
+    }
+
+    [Fact]
     public async void EmptyNameSearchIsBad()
     {
         var accessService = new Mock<IAccessService>();
@@ -55,6 +75,22 @@ public class SongController_Tests
     }
 
     [Fact]
+    public async void NameSearchReturnsSacrifices()
+    {
+        var accessService = new AccessService();
+
+        var controller = new SongController(accessService);
+
+        var response = await controller.Search("sacrifices");
+        var castResponse = response as OkObjectResult;
+        var responseContent = castResponse.Value as string;
+        var jsonContent = JObject.Parse(responseContent);
+        var name = jsonContent["tracks"]["items"][0]["name"];
+
+        Assert.Equal("Sacrifices (with EARTHGANG & J. Cole feat. Smino & Saba)", name);
+    }
+
+    [Fact]
     public async void EmptyGetFeaturesBad()
     {
         var accessService = new Mock<IAccessService>();
@@ -68,5 +104,21 @@ public class SongController_Tests
         responseContent = castResponse.Value.ToString();
 
         Assert.Equal("No song given", responseContent);
+    }
+
+    [Fact]
+    public async void HumbleFeaturesCorrect()
+    {
+        var accessService = new AccessService();
+
+        var controller = new SongController(accessService);
+
+        var response = await controller.Features("131OLY5J8XyfGuSjXRiTRM");
+        var castResponse = response as OkObjectResult;
+        var responseContent = castResponse.Value as string;
+        var jsonContent = JObject.Parse(responseContent);
+        var tempo = jsonContent["tempo"];
+
+        Assert.Equal(150.039, tempo);
     }
 }
