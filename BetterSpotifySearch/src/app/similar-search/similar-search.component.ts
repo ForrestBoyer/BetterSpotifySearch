04cece41f2598346431274cce18fb45f1e4459c6
fboyer <forrestboyer2@gmail.com>
2024-04-22 14:52:07 -0400

    in the middle of making similar search work

--- BetterSpotifySearch/src/app/similar-search/similar-search.component.ts
+++ BetterSpotifySearch/src/app/similar-search/similar-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SongNameIdService } from '../song-name-id.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-similar-search',
@@ -6,25 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./similar-search.component.scss']
 })
 export class SimilarSearchComponent implements OnInit {
+  public songInfo?: string;
   public songName?: string;
-  public songID?: number;
-  public danceability?: number;
-  public timeSignature?: number;
-  public instrumentalValue?: number;
-  public liveliness?: number;
-  public loudness?: number;
-  public mode?: number;
-  public speechiness?: number;
-  public acousticness?: number;
-  public energy?: number;
-  public valence?: number;
-  public tempo?: number;
-  public duration?: number;
-  public genre?: string;
+  public songID?: string;
+  public songLink?: string;
+  public parse?: any
 
-  constructor() { }
+  constructor(protected http: HttpClient, 
+              private nameID: SongNameIdService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    
   }
 
   setSongName(event: any): void {
@@ -32,9 +28,47 @@ export class SimilarSearchComponent implements OnInit {
     console.log("Song Name: ", this.songName);
   }
 
-  searchSimilarSongs() {
-    // Call api for similar songs w/ features
-    console.log("Requesting songs similar to: ", this.songName);
+  searchSimilarSongs()
+  {
+    /*
+      1. Get song
+      2. Search with that song to get its info
+      3. Search again with that info, using the options the user selected
+      4. Route to show that data
+    */
   }
 
+  searchSimilarSongs() {
+    // api call for song search
+    // GET http://localhost:40080/api/Song/Search/{{songName}}
+    console.log("Song info request: ", this.songName)
+    var url = this.createUrl();
+
+    this.http.get<string>(url).subscribe(result => 
+      {
+        // *** Parse string to get returned song id ***
+        this.songInfo = result;
+        this.parse = JSON.stringify(this.songInfo);
+        this.parse = JSON.parse(this.parse);
+        this.songID = this.parse["tracks"]["items"]["0"]["id"];
+        this.songName = this.parse["tracks"]["items"]["0"]["name"];
+        this.songLink = this.parse["tracks"]["items"]["0"]["external_urls"]["spotify"];
+
+        // Pass song name and ID to info results page through service
+        this.nameID.setSongId(this.songID);
+        this.nameID.setSongName(this.songName);
+        this.nameID.setSongLink(this.songLink);
+        this.router.navigateByUrl('/info-results')
+
+        console.log(this.songInfo);
+        // console.log("Parse: ", this.parse);
+        // console.log("Parsed Song ID: ", this.songID);
+      }, error => console.error(error));
+  }
+
+  createUrl(): string {
+    var url:string = "api/song/searchby/";
+
+
+  }
 }
